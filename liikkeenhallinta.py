586420384b08c727501315db35813f6c6f226530
Teemu Mäenpää <teemumaen@gmail.com>
2020-10-02 19:42:18 +0300

    koordinaattien kulma radiaaneiksi aj kysy_liike

--- liikkeenhallinta.py
+++ liikkeenhallinta.py
@@ -15,9 +15,23 @@ hahmo_2 = {
 }
 
 def muunna_xy_koordinaateiksi(kulma,vektori):
- #asteet = int(kulma*180/math.pi)
- x0 = round(vektori*math.cos(kulma), 0)
- y0 = round(vektori*math.sin(kulma), 0)
+ kulma_radiaanit = math.radians(kulma)
+ x0 = round(vektori*math.cos(kulma_radiaanit), 0)
+ y0 = round(vektori*math.sin(kulma_radiaanit), 0)
  x = int(x0)
  y = int(y0)
  return x, y
+
+def kysy_liike(hahmo):
+    x0 = hahmo["x"]
+    y0 = hahmo["y"]
+    print("Hahmo on sijainnissa ({}, {})", x, y)
+    suunta = float(input("Anna liikkumissuunta asteina: "))
+    nopeus = float(input("Anna liikenopeus: "))
+    hahmo["suunta"] = suunta
+    hahmo["nopeus"] = nopeus
+
+
+def paivita_sijainti(hahmo):
+    nopeus = hahmo["nopeus"]
+    suunta = hahmo["suunta"]
